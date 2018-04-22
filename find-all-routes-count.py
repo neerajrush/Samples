0e5c905522d2a81e0ad8cf8a76866ce52bbd1077
neershar <neershar@cisco.com>
2018-04-22 12:35:04 -0700

    -a

--- find-all-routes-count.py
+++ find-all-routes-count.py
@@ -8,32 +8,50 @@ def is_blocked(i, j):
 		if i ==  blocked[x].dx and j == blocked[x].dy:
 			return True
 
-def path(i, j , M, N, blocked):
+def is_path(i, j , M, N, blocked):
 	if i == M - 1 and j == N - 1:
-		return 1
+		return True
 
 	if is_blocked(i, j):
-		return 0
+		return False
 
         # right direction already blocked (i == M -1)
 	if j == N-1 and is_blocked(i+1, j):
-		return 0
+		return False
 
         # down direction already blocked ( j == N -1)
 	if i == M-1 and is_blocked(i, j+1):
-		return 0
+		return False
 
-	return 1
+	return True
+
+#
+# Input: number of paths from (0, 0) to (5, 5):
+# [[0, 0, 0, 0, 0, 0], 
+#  [0, 0, 0, 0, 0, X],   blocked: (1, 5)
+#  [0, 0, 0, X, 0, 0],   blocked: (2, 3)
+#  [0, 0, 0, 0, 0, 0], 
+#  [0, X, 0, X, 0, 0],   blocked: (4, 1), (4, 3)
+#  [0, 0, 0, 0, 0, 0]]
+#
+#
+# Result:
+# Count:  61
+# [[61, 32, 16, 8, 4, 0], 
+#  [29, 16,  8, 4, 4, 0], 
+#  [13,  8,  4, 0, 4, 1], 
+#  [ 5,  4,  4, 3, 3, 1], 
+#  [ 1,  0,  1, 0, 2, 1], 
+#  [ 1,  1,  1, 1, 1, 1]]
 
 if __name__ == "__main__":
 	M = 6
 	N = 6
-	blocked = [ point(2, 3), point(1, 5), point(4, 1), point(4, 3) ]
+	blocked = [ point(1, 5), point(2, 3), point(4, 1), point(4, 3) ]
 	grid = [ [0 for j in range(N) ] for i in range(M) ]
 	for i in range(M)[::-1]:
 		for j in range(N)[::-1]:
-			px = path(i, j, M, N, blocked)
-			if px:
+			if is_path(i, j, M, N, blocked):
 				if i+1 < M and j+1 < N:
 					grid[i][j] = grid[i+1][j] + grid[i][j+1]
 				else:
